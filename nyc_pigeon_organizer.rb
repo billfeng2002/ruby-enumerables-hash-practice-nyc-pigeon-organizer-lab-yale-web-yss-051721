4901cbb4769d7bf6d9c0ad8125a2486b5cc2d227
Bill Feng <bill.feng@yale.edu>
2021-05-25 00:32:17 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,11 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  newHash={}
+  data.each{
+    |attributeName, attributeValue|
+    attributeValue.each{
+      |attribute_val, name|
+      newHash[name][attributeName]=attribute_val
+    }
+  }
 end
